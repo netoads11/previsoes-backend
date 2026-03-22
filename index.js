c4d96699704b24eec726165cf6b550cff0f2168f
netoads11 <netoads11@gmail.com>
2026-03-22 12:25:52 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ status: 'ok', message: 'Previsoes API rodando!' });
+});
+
+const authRoutes = require('./src/routes/auth');
+const marketRoutes = require('./src/routes/markets');
+const betRoutes = require('./src/routes/bets');
+const walletRoutes = require('./src/routes/wallet');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/markets', marketRoutes);
+app.use('/api/bets', betRoutes);
+app.use('/api/wallet', walletRoutes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log('Servidor rodando na porta ' + PORT);
+});
+
+module.exports = app;
